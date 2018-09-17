d3ed50e203788acdcee5466fc96cfce851d72f02
Dan Cortes <danielgcortes@gmail.com>
2018-09-16 22:56:25 -0500

    Fix styles on Android

--- views/Home/Home.react.js
+++ views/Home/Home.react.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   View,
   ScrollView,
   StyleSheet
 } from 'react-native';
 import {
-  Header,
   Text,
-  Icon,
-  Button
+  Icon
 } from 'react-native-elements';
 import {userStore} from 'stores/User/User.store';
 import Logo from 'components/Logo/Logo.react';
@@ -51,22 +50,6 @@ export default class Home extends React.Component {
     )
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={{flex: 1}}>
-          <Text>
-            Hello, {userStore.user.email}!
-          </Text>
-        </ScrollView>
-        <HomeActions navigation={this.props.navigation} />
-      </View>
-    );
-  }
-}
-
-class HomeActions extends React.Component {
-
   createEvent = () => {
     this.props.navigation.navigate('EventDetails')
   };
@@ -74,12 +57,17 @@ class HomeActions extends React.Component {
   render() {
     return (
       <View style={{
-        flexDirection: 'row',
-        borderTopWidth: 2,
-        borderTopColor: 'rgba(0, 0, 0, 0.1)',
-        zIndex: 1
+        alignItems: 'stretch',
+        backgroundColor: '#fff',
+        flex: 1,
+        justifyContent: 'flex-start'
       }}>
-        <View style={{width: '50%'}}>
+        <ScrollView style={{flex: 1}}>
+          <Text>
+            Hello, {userStore.user.email}!
+          </Text>
+        </ScrollView>
+        <BottomButtonRow>
           <Icon
             name='schedule'
             color={vars.colors.purple}
@@ -91,28 +79,6 @@ class HomeActions extends React.Component {
             }}
             onPress={() => console.log('tapped plans')}
           />
-        </View>
-        <View
-          pointerEvents='box-none'
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            width: '100%',
-            alignItems: 'center',
-            zIndex: 1
-          }}
-        >
-          <Icon
-            reverse
-            raised
-            name='add'
-            underlayColor='transparent'
-            color={vars.colors.purple}
-            onPress={this.createEvent}
-          />
-        </View>
-        <View style={{width: '50%'}}>
           <Icon
             name='inbox'
             color={vars.colors.purple}
@@ -124,17 +90,66 @@ class HomeActions extends React.Component {
             }}
             onPress={() => console.log('tapped inbox')}
           />
-        </View>
+        </BottomButtonRow>
+        <AddEventButton
+          onPress={this.createEvent}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'stretch',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'flex-start'
+class AddEventButton extends React.Component {
+
+  static propTypes = {
+    onPress: PropTypes.func
   }
-});
+
+  render() {
+    return (
+      <View
+        pointerEvents='box-none'
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          width: '100%',
+          alignItems: 'center',
+          zIndex: 1
+        }}
+      >
+        <Icon
+          reverse
+          raised
+          name='add'
+          underlayColor='transparent'
+          color={vars.colors.purple}
+          onPress={this.props.onPress}
+        />
+      </View>
+    );
+  }
+}
+
+class BottomButtonRow extends React.Component {
+  render() {
+    const width = `${100 / this.props.children.length}%`;
+    return (
+      <View style={{
+        flexDirection: 'row',
+        borderTopWidth: 2,
+        borderTopColor: 'rgba(0, 0, 0, 0.1)',
+        zIndex: 1
+      }}>
+        {this.props.children.map((child, i) => (
+          <View
+            style={{width}}
+            key={i}
+          >
+            {child}
+          </View>
+        ))}
+      </View>
+    );
+  }
+}
